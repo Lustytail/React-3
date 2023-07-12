ec0fb289955bde6d480c0ee5c2f19e4fb43b0dac
Lustytail <qkrehdgns91@gmail.com>
2023-07-12 18:26:49 +0900

    animation update

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,8 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme ={
-    bgColor: "#2d3436",
-    blkColor: "whitesmoke",
-    textColor: "#dfe6e9",
-    accentColor: "#6c5ce7"
-}
+export const darkTheme: DefaultTheme = {};
